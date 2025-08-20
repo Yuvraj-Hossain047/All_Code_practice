762965b09e3a72100894d2e3a524ca62e1f59dd9
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-20 13:31:45 +0600

    Create Age Calculator.c

---
+++ C Noob/Age Calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{   int a,b;
+
+    printf("what is your birth year ?\n");
+    scanf("%d", &a);
+    printf("what is the current year ?\n");
+    scanf("%d", &b);
+
+    printf(" your age is %d\n", b-a);
+return 0;
+}
