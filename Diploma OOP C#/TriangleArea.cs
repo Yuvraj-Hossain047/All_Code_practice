0f69069a50c8372630dd4499ba37f442ca2c96eb
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-20 14:28:17 +0600

    Update TriangleArea.cs

--- Diploma OOP C#/TriangleArea.cs
+++ Diploma OOP C#/TriangleArea.cs
@@ -6,20 +6,20 @@ namespace Area
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the First Number:");
+            Console.WriteLine("Enter the First Number");
             double a = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the Second Number:");
+            Console.WriteLine("Enter the second Number");
             double b = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter the Third Number:");
+            Console.WriteLine("Enter the Third Number");
             double c = Convert.ToDouble(Console.ReadLine());
 
             if ((a + b > c) && (b + c > a) && (c + a > b))
             {
                 double s = (a + b + c) / 2;
-                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
-                Console.WriteLine("The area of the triangle is: " + area.ToString("F2"));
+                double result = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                Console.WriteLine("The result is " + result);
             }
             else
             {
