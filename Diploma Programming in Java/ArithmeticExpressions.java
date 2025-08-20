12db6322e5fd30b8fe0113176613d7eabe70ecb3
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-20 13:48:00 +0600

    Create ArithmeticExpressions.java

---
+++ Diploma Programming in Java/ArithmeticExpressions.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+        // no need for Scanner, since no input is taken
+        int expr1, expr2, expr3, expr4;
+
+        expr1 = -5 + 8 * 6;
+        expr2 = (55 + 9) % 9;
+        expr3 = 20 + (-3 * 5 / 8);
+        expr4 = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println("Expression 1: " + expr1);
+        System.out.println("Expression 2: " + expr2);
+        System.out.println("Expression 3: " + expr3);
+        System.out.println("Expression 4: " + expr4);
+    }
+}
