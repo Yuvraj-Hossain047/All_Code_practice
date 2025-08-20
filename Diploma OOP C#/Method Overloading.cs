bbd196910e7d0d531687bf065f1107c67ea4b93b
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-20 21:02:21 +0600

    Update Method Overloading.cs

--- Diploma OOP C#/Method Overloading.cs
+++ Diploma OOP C#/Method Overloading.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace ConsoleApplication15
 {
-    public class sum
+    public class Sum
     {
-        public int A(int a, int b)
+        public int SumNumbers(int a, int b)          // adds 2 numbers
         {
             return a + b;
         }
 
-        public int B(int a, int b, int c)
+        public int SumNumbers(int a, int b, int c)   // adds 3 numbers
         {
             return a + b + c;
         }
@@ -19,10 +19,10 @@ namespace ConsoleApplication15
     {
         static void Main(string[] args)
         {
-            sum obj = new sum();
+            Sum obj = new Sum();
 
-            int sum_1 = obj.A(5, 10);
-            int sum_2 = obj.B(10, 5, 25);
+            int sum_1 = obj.SumNumbers(5, 10);        // calls 2-parameter method
+            int sum_2 = obj.SumNumbers(10, 5, 25);    // calls 3-parameter method
 
             Console.WriteLine("First Result is = " + sum_1);
             Console.WriteLine("Second Result is = " + sum_2);
