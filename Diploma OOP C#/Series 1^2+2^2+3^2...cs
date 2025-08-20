1d69e53d439f751d04d5854805b2194828020501
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-20 21:05:09 +0600

    Create Series 1^2+2^2+3^2...cs

---
+++ Diploma OOP C#/Series 1^2+2^2+3^2...cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace TwoNumberAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Minimum Number");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Maximum Number");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                sum = sum + i * i;
+            }
+
+            Console.WriteLine("The Result is " + sum);
+            Console.ReadKey();
+        }
+    }
+}
