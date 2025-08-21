5e4c0459ba7738a47e8d35d92118c3b1c2ca4b82
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-21 12:53:06 +0600

    Create CPP Hello World.cpp

---
+++ Object Oriented Programming/CPP Hello World.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int x = 100;
+    char y = 'A';
+    bool b = true;
+    string s = "america ya";
+
+    cout << x << endl
+         << y << endl
+         << b << endl
+         << s << endl
+         << "hallo" << endl;
+
+    return 0;
+}
