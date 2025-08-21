0de1a7167e67280ae901ce69c99d2b88ab33bfc5
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-21 12:48:02 +0600

    Create CPP SizeOf fun*.cpp

---
+++ Object Oriented Programming/CPP SizeOf fun*.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(char)   << " byte"  << endl
+         << sizeof(bool)   << " byte"  << endl
+         << sizeof(int)    << " bytes" << endl
+         << sizeof(float)  << " bytes" << endl
+         << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
