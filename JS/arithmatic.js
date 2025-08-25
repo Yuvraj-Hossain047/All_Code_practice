efc0c2bd7ca6ae87d4aff36bd6c0c4ad664327a7
Mansur Hossain Yuvraj <119665190+Yuvraj-Hossain047@users.noreply.github.com>
2025-08-25 22:16:54 +0600

    Create arithmatic.js

---
+++ JS/arithmatic.js
@@ -0,0 +1,14 @@
+let a= 5; 
+let b= 6;
+
+console.log("a= ",a , "& b= ", b);
+console.log("a+b= ", a+b);
+console.log("a-b= ", a-b);
+console.log("a*b= ", a*b);
+console.log("a/b= ", a/b);
+console.log("a%b= ", a%b);
+console.log("a pow b= ", a**b);
+console.log("++a= ", ++a);
+console.log("--a= ", --a);
+console.log( a++, " =a");
+console.log( a--, " =a");
